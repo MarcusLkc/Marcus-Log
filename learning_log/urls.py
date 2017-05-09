4ff2eb69867ce0b59e21226b9c8abc144f3e576f
marcuslkc <marcuscrowder66@gmail.com>
2017-05-09 13:45:57 -0400

    yeh

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^coolbeansadmin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'', include('learning_logs.urls', namespace='learning_logs')),
     
